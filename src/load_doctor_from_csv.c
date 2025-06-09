379991de2660c63ea67bb128ceb67a2cc838b029
Rifki Afriadi <166176893+rifkiiiafdd@users.noreply.github.com>
2025-06-09 15:35:58 +0700

    editting comment

--- src/load_doctor_from_csv.c
+++ src/load_doctor_from_csv.c
@@ -1,12 +1,12 @@
 /*
 spesifikasi : Mengambil data dokter dari file csv dan memasukkannya ke dalam linked list
 input  : nama file (string)
-output : linked list dokter
+output : array of structurs dokter
 */
 
 #include <stdio.h>
 
 int main() {
-    
+
     return 0;
 }
---
+++ src/saveDoctorToCSV.c
@@ -0,0 +1,5 @@
+/*
+spesifikasi : Menyimpan data dokter ke file csv l
+input  : array of structurs
+output : file csv yang sudah terisi
+*/
